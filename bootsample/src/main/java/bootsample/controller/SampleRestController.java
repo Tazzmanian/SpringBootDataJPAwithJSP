4951b63deb4ccf51fa629c837c5428c5259d0491
Tazzmanian <banskaliqtabg@yahoo.com>
2017-01-13 03:29:08 +0200

    Works!!! except for bootstrap cant make it work

--- bootsample/src/main/java/bootsample/controller/SampleRestController.java
+++ bootsample/src/main/java/bootsample/controller/SampleRestController.java
@@ -21,21 +21,21 @@ public class SampleRestController {
 		return "Hello World Sample rest controller";
 	}
 	
-	@GetMapping("/all-tasks")
-	private String allTasks() {
-		return taskService.findAll().toString();
-	}
-	
-	@GetMapping("/save-task")
-	public String saveTask(@RequestParam String name, @RequestParam String desc) {
-		Task task = new Task(name, desc, new Date(), false);
-		taskService.save(task);
-		return "Task saved!";
-	}
-	
-	@GetMapping("/delete-task")
-	public String saveTask(@RequestParam int id) {
-		taskService.delete(id);
-		return "Task deleted!";
-	}
+//	@GetMapping("/all-tasks")
+//	private String allTasks() {
+//		return taskService.findAll().toString();
+//	}
+//	
+//	@GetMapping("/save-task")
+//	public String saveTask(@RequestParam String name, @RequestParam String desc) {
+//		Task task = new Task(name, desc, new Date(), false);
+//		taskService.save(task);
+//		return "Task saved!";
+//	}
+//	
+//	@GetMapping("/delete-task")
+//	public String saveTask(@RequestParam int id) {
+//		taskService.delete(id);
+//		return "Task deleted!";
+//	}
 }
--- bootsample/src/main/java/bootsample/service/TaskService.java
+++ bootsample/src/main/java/bootsample/service/TaskService.java
@@ -28,6 +28,10 @@ public class TaskService {
 		return tasks;
 	}
 	
+	public Task findTask(int id) {
+		return taskRepository.findOne(id);
+	}
+	
 	public void save(Task task) {
 		taskRepository.save(task);
 	}
